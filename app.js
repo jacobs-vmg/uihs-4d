8935573a34bcc5e9b3c7480f1ef617d1af658d45
David <onesimusrex@gmail.com>
2022-02-02 09:45:19 -0500

    add images and 360 viewer

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public/uihsphasing')));
+// app.use(express.static(path.join(__dirname, 'public/uihsphasing')));
+app.use(express.static(path.join(__dirname, 'public/uihsphasing/utils/360-image-slider/dist/')));
 // app.use(express.static(path.join(__dirname, 'public/offlineves/Archive')));
 
 app.use('/', indexRouter);
---
+++ public/uihsphasing/utils/360-image-slider/README.markdown
@@ -0,0 +1,5 @@
+# 360 Image Slider
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/alyda/pen/MWoZqz](https://codepen.io/alyda/pen/MWoZqz).
+
+https://github.com/heartcode/360-Image-Slider
